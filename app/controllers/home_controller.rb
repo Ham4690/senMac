804d526d906d86c31fc78019d4894d48eb8cb15c
Ham4690 <ham46903@gmail.com>
2020-05-23 07:16:20 +0900

    render test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def top
+    @basket = nil
+    puts @basket
+  end
+
+  def select
+    @basket = 1
+    puts @basket
+    render "top"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "home#top"
+
   get '/' => "home#top"
+  get '/home' => "home#top"
+  get '/select' => "home#select"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
